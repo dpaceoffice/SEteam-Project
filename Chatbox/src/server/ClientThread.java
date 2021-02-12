5a1436936ab7ef79fdc5e61694c2f4e1a9da9556
dpaceoffice <dpaceoffice@gmail.com>
2021-02-12 10:03:57 -0600

    Instance variables and constructor

--- Chatbox/src/server/ClientThread.java
+++ Chatbox/src/server/ClientThread.java
@@ -5,3 +5,18 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
+
+public class ClientThread extends Server implements Runnable {
+	private Socket socket;
+	private PrintWriter outPrintWrapper;
+	private DataOutputStream outDataWrapper;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param socket = open socket
+	 */
+	public ClientThread(Socket socket) {
+		this.socket = socket;
+	}
+
