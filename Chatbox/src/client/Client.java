941499f4e7abea95f7dc337f673d48d47600756e
dpaceoffice <dpaceoffice@gmail.com>
2021-02-12 09:58:12 -0600

    Instance variables for client

--- Chatbox/src/client/Client.java
+++ Chatbox/src/client/Client.java
@@ -3,3 +3,24 @@ package client;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
+/**
+ * Main client class
+ */
+public class Client {
+	/**
+	 * Host ip address
+	 */
+	private static final String HOST = "127.0.0.1";
+	
+	/**
+	 * Open port to connect to
+	 */
+	private static final int PORT = 43594;
+
+	/**
+	 * State variables
+	 */
+	private String name;
+	private String ipAddress;
+	private int port;
+	private Scanner scan;
