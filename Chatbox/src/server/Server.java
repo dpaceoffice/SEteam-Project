5b0405f08638c02e31e47ed55c3d25dd8226c1dc
dpaceoffice <dpaceoffice@gmail.com>
2021-02-12 10:11:52 -0600

    Main method starting server socket and listening for client connections

--- Chatbox/src/server/Server.java
+++ Chatbox/src/server/Server.java
@@ -12,3 +12,28 @@ public class Server {
 	 */
 	public static ArrayList<ClientThread> clients;
 
+	@SuppressWarnings("resource")
+	public static void main(String args[]) {
+		try {
+			// initilize arraylist maintaing active threads
+			clients = new ArrayList<ClientThread>();
+
+			// initilize the server on port 43594
+			ServerSocket serverSocket = new ServerSocket(43594);
+			System.out.println("Listening for connections on port 43594");
+
+			// Loop indefinitly till program is exited
+			while (true) {
+				Socket clientSocket = serverSocket.accept();// accepts any incomming connections
+				System.out.println("Accepted Connection: " + clientSocket.getInetAddress() + "");
+				ClientThread client = new ClientThread(clientSocket);// initates new thread for accepted connection
+				new Thread(client).start();// starts the thread
+				clients.add(client);// adds thread to arraylist
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
