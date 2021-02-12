ef54fab0080a1eaadf4bec3792da232d752358d5
dpaceoffice <dpaceoffice@gmail.com>
2021-02-12 10:11:27 -0600

    Static arraylist maintaining connected clients

--- Chatbox/src/server/Server.java
+++ Chatbox/src/server/Server.java
@@ -6,3 +6,9 @@ import java.net.Socket;
 import java.util.ArrayList;
 
 public class Server {
+
+	/**
+	 * Arraylist to keep track of active threads
+	 */
+	public static ArrayList<ClientThread> clients;
+
