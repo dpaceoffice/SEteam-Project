ce44ae14726da90d3600f826910fb8f81b6b8276
dpaceoffice <dpaceoffice@gmail.com>
2021-02-12 10:01:23 -0600

    Instance variables and constructor

--- Chatbox/src/client/ServerThread.java
+++ Chatbox/src/client/ServerThread.java
@@ -8,3 +8,26 @@ import java.net.Socket;
 import java.util.LinkedList;
 import java.util.Scanner;
 
+/**
+ * ServerThread is a thread that handles all communications with the server
+ */
+public class ServerThread implements Runnable {
+
+	/**
+	 * State variables
+	 */
+	private Socket socket;
+	private String userName;
+	private final LinkedList<String> queuedMessages;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param socket   - socket used to connect to server
+	 * @param userName - display name picked by user
+	 */
+	public ServerThread(Socket socket, String userName) {
+		this.socket = socket;
+		this.userName = userName;
+		queuedMessages = new LinkedList<String>();
+	}
