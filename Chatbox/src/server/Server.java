bf7687926b20c02680689d568ba5a58e607d7752
dpaceoffice <dpaceoffice@gmail.com>
2021-03-05 11:38:35 -0600

    Properly reaps clients that early disconnect

--- Chatbox/src/server/Server.java
+++ Chatbox/src/server/Server.java
@@ -4,6 +4,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.ArrayList;
 
 public class Server {
@@ -12,6 +13,7 @@ public class Server {
 	 * Arraylist to keep track of active threads
 	 */
 	public static ArrayList<ClientThread> clients;
+	private ClientThread last = null;
 
 	@SuppressWarnings("resource")
 	public static void main(String args[]) {
@@ -38,18 +40,29 @@ public class Server {
 		}
 	}
 
-	public void distributeMessage(String msg) throws IOException {
-		// send input as output to all active threads including this one with user name
-		//BufferedWriter b_out = null;
-		DataOutputStream d_out = null;
-		for (ClientThread client : clients) {
-			// get current threads output stream
-			//b_out = client.getbWriter();
-			d_out = client.getdOutputStream();
-			if (d_out != null) {
-				d_out.writeInt(1);// String
-				d_out.writeBytes(msg + "\r\n");
+	public void distributeMessage(String msg) {
+		try {
+			// send input as output to all active threads including this one with user name
+			// BufferedWriter b_out = null;
+			DataOutputStream d_out = null;
+			for (ClientThread client : clients) {
+				// get current threads output stream
+				// b_out = client.getbWriter();
+				this.last = client;//keeps track of thread in case of exception
+				d_out = client.getdOutputStream();
+				if (d_out != null) {
+					d_out.writeInt(1);// String packet
+					d_out.writeBytes(msg + "\n");
+				}
 			}
+		} catch (IOException e) {
+			if (e instanceof SocketException) // if this happens its probably because the client closed before the server reaped
+												// the thread.
+				if (e.getMessage().contains("Connection reset by peer")) {
+					clients.remove(last);
+					return;
+				}
+			e.printStackTrace();
 		}
 	}
 
