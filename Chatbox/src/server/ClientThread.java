3cd586c58a94e086ceaac995cc4675025ecf24ab
dpaceoffice <dpaceoffice@gmail.com>
2021-03-05 11:40:46 -0600

    imports

--- Chatbox/src/server/ClientThread.java
+++ Chatbox/src/server/ClientThread.java
@@ -6,7 +6,6 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
-import java.net.SocketException;
 
 public class ClientThread extends Server implements Runnable {
 
