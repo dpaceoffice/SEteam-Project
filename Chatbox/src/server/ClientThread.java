5a5d8e6ad149041e800c75adc51872be417d1a6a
dpaceoffice <dpaceoffice@gmail.com>
2021-03-04 20:23:23 -0600

    Necassary imports

--- Chatbox/src/server/ClientThread.java
+++ Chatbox/src/server/ClientThread.java
@@ -1,10 +1,12 @@
 package server;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.InputStreamReader;
 import java.net.Socket;
-import java.util.Scanner;
+import java.net.SocketException;
 
 public class ClientThread extends Server implements Runnable {
 	private Socket socket;
