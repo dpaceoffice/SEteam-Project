804736d171960818f81aa7eaa4ab2fa51ac7bfa4
dpaceoffice <dpaceoffice@gmail.com>
2021-03-04 20:22:05 -0600

    Necessary Imports

--- Chatbox/src/client/ServerThread.java
+++ Chatbox/src/client/ServerThread.java
@@ -1,12 +1,11 @@
 package client;
 
+import java.io.BufferedReader;
 import java.io.DataInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintWriter;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.LinkedList;
-import java.util.Scanner;
 
 /**
  * ServerThread is a thread that handles all communications with the server
