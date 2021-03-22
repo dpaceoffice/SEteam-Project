b24dbd8f260b3e29832d17c5f614358662fe8af3
dpaceoffice <dpaceoffice@gmail.com>
2021-03-22 01:48:13 -0500

    Packet Refractor/additional packets

--- Chatbox/src/server/Threading/Packet.java
+++ Chatbox/src/server/Threading/Packet.java
@@ -4,6 +4,7 @@ public class Packet {
     public static int MESSAGE_PACKET = 1;
     public static int USERNAME_REQ = 2;
     public static int LOGIN_CHECK = 3;
-    public static int IDLE_PACKET = 1000;
-    
+    public static int STATE_CHANGE = 4;
+    public static int DISCONNECT_PACKET = 1000;
+    public static int IDLE_PACKET = 1001;
 }
