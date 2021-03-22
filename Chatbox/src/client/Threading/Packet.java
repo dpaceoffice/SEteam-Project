c87fa88f1cfb09c5fc03ebf042429372ed5a9064
dpaceoffice <dpaceoffice@gmail.com>
2021-03-21 20:50:43 -0500

    Packet Constants

---
+++ Chatbox/src/client/Threading/Packet.java
@@ -0,0 +1,8 @@
+package client.Threading;
+
+public class Packet {
+    public static int MESSAGE_PACKET = 1;
+    public static int LOGIN_CHECK = 3;
+    public static int IDLE_PACKET = 1000;
+    
+}
