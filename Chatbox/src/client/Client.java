0b0e73efb7058de9042501aacb9ef77616ef0945
dpnshu3 <70821134+dpnshu3@users.noreply.github.com>
2021-04-03 18:12:28 -0500

    add encryption to user password

--- Chatbox/src/client/Client.java
+++ Chatbox/src/client/Client.java
@@ -23,6 +23,8 @@ public class Client {
 	/**
 	 * State variables
 	 */
+	private String name;
+	private String password;
 	private String ipAddress;
 	private int port;
 	private Scanner scan;
@@ -52,12 +54,47 @@ public class Client {
 		}
 		this.name = name;
 	}*/
+	private void reqName() {
+		System.out.println("Please input username:");
+		while (this.name == "" || this.name.isEmpty()) {
+			// null, empty, whitespace(s) not allowed.
+			this.name = this.scan.nextLine();
+			if (name.isEmpty()) {
+				System.out.println("Invalid. Please enter again:");
+			}
+		}
+	}
+
+	private void reqPassword() {
+		System.out.println("Please input password:");
+		while (this.password == "" || this.password.isEmpty()) {
+			// null, empty, whitespace(s) not allowed.
+			this.password = this.scan.nextLine();
+			if (this.password.isEmpty() || checkPasswordRequirement(this.password)) {
+				System.out.println("Invalid. Please enter again:");
+			}
+		}
+	}
+
+
+// 	(?=.*[0-9]) a digit must occur at least once
+// (?=.*[a-z]) a lower case letter must occur at least once
+// (?=.*[A-Z]) an upper case letter must occur at least once
+// (?=.*[@#$%^&+=]) a special character must occur at least once
+// (?=\\S+$) no whitespace allowed in the entire string
+// .{8,} at least 8 characters
+
+	private boolean checkPasswordRequirement(String password) {
+		String pattern = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}";
+		return password.matches(pattern);
+	}
 
 	/**
 	 * Starts the thread that listens for messages from server Also keeps track of
 	 * keyboard input on client's parent thread (Client) to send to child thread
 	 * (ServerThread)
 	 */
+	
 	private void startClient() {
 		try {
 			Socket socket = new Socket(ipAddress, port);
@@ -80,6 +117,8 @@ public class Client {
 		Client client = new Client(HOST, PORT);
 		//client.reqName();
 		client.startClient();
+		client.reqName();
+		client.reqPassword();
 	}
 
 }
